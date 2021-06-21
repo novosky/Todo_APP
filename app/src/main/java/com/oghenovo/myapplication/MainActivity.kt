2fd84447afc052ebe65dbef00a0f5bc8fc821bba
novosky <oghenovoegbegbedia@gmail.com>
2021-06-21 15:04:22 +0100

    Todo APP

--- app/src/main/java/com/oghenovo/myapplication/MainActivity.kt
+++ app/src/main/java/com/oghenovo/myapplication/MainActivity.kt
@@ -2,10 +2,23 @@ package com.oghenovo.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    val username = "admin"
+    val password = "admin"
+
+    private lateinit var usernameInput:EditText
+    private lateinit var passwordInput:EditText
+    private lateinit var submitButton:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        
     }
 }
